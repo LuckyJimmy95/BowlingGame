fb01c9e3ead85a364849660fb4ad402f638974de
JS <jsierrani@ltu.edu>
2021-05-04 00:42:17 -0400

    Added test 1

---
+++ BowlingGame/BowlingGameTests/BowlingGameTest.cs
@@ -0,0 +1,20 @@
+﻿using BowlingGame;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace BowlingGameTests
+{
+    [TestClass]
+    public class BowlingGameTest
+    {
+        [TestMethod]
+        public void TestGutterGame()
+        {
+            Game g = new Game();
+            for (int i = 0; i < 20; i++)
+                g.Roll(0);
+
+            Assert.AreEqual(0, g.Score(), "Score expected to be 0");
+
+        }
+    }
+}
