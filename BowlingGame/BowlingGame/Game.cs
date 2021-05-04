af588c5e92ed3b64612989a69df130aec9b363cb
JS <jsierrani@ltu.edu>
2021-05-04 00:50:04 -0400

    Test 2 functionality

--- BowlingGame/BowlingGame/Game.cs
+++ BowlingGame/BowlingGame/Game.cs
@@ -1,20 +1,17 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace BowlingGame
+﻿namespace BowlingGame
 {
     public class Game
     {
+        private int _score = 0;
+
         public void Roll(int pins)
         {
+            _score += pins;
         }
 
         public int Score()
         {
-            return 0;
+            return _score;
         }
 
     }
--- BowlingGame/BowlingGameTests/BowlingGameTest.cs
+++ BowlingGame/BowlingGameTests/BowlingGameTest.cs
@@ -6,14 +6,35 @@ namespace BowlingGameTests
     [TestClass]
     public class BowlingGameTest
     {
+        private Game _game;
+
+        [TestInitialize]
+        public void SetUp()
+        {
+            _game = new Game();
+        }
+
         [TestMethod]
         public void TestGutterGame()
         {
             Game g = new Game();
             for (int i = 0; i < 20; i++)
-                g.Roll(0);
+                _game.Roll(0);
 
-            Assert.AreEqual(0, g.Score(), "Score expected to be 0");
+            Assert.AreEqual(0, _game.Score(), "Score expected to be 0");
+        }
+
+        [TestMethod]
+        public void TestAllOnes()
+        {
+            RollMany(20, 1);
+            Assert.AreEqual(20, _game.Score(), "Score expected to be 20");
+        }
+
+        private void RollMany(int n, int pins)
+        {
+            for (int i = 0; i < n; i++)
+                _game.Roll(pins);
 
         }
     }
